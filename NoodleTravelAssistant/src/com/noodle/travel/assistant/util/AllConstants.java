94e7d632df17682bcd1bc8f3f3f55a46214bbfb7
DelgadoDing <yiren.ding@Hp.com>
2014-04-14 13:26:13 +0800

    update for 景点美图，景点游记

--- NoodleTravelAssistant/src/com/noodle/travel/assistant/util/AllConstants.java
+++ NoodleTravelAssistant/src/com/noodle/travel/assistant/util/AllConstants.java
@@ -4,13 +4,14 @@ public class AllConstants {
 
 	public static final String WEIXIN_IMAGE_MESSAGE = "<xml><MsgType>image</MsgType> <PicUrl>noodlePicUrl</PicUrl></xml>";
 	
-	public static final String WEI_XIN_IMAGE_ITEM = "<item><Title></Title><Description>图片number</Description><PicUrl>noodlePicUrl</PicUrl><Url>noodleClickUrl</Url></item>";
+	public static final String WEI_XIN_IMAGE_ITEM = "<item><Title>noodelImageNumber</Title><Description>noodelImageNumber</Description><PicUrl>noodlePicUrl</PicUrl><Url>noodleClickUrl</Url></item>";
 	public static final String WEI_XIN_ARTICLES_MESSAGE_START = "<xml><MsgType>news</MsgType><ArticleCount>noodleArticleCount</ArticleCount><Articles>";
 	public static final String WEI_XIN_ARTICLES_MESSAGE_END = "</Articles><FuncFlag>1</FuncFlag></xml> ";
 	
 	public static final String NOODLE_PIC_URL = "noodlePicUrl";
 	public static final String NOODLE_ARTICLE_COUNT = "noodleArticleCount";
-	public static final String NOODLE_IMAGE_NUMBER = "图片number";
+	public static final String NOODLE_IMAGE_NUMBER = "noodelImageNumber";
+	public static final String NOODLE_IMAGE_DESC = "noodelImageNumber";
 	public static final String NOODLE_IMAGE_CLICK_URL = "noodleClickUrl";
 	
 }
