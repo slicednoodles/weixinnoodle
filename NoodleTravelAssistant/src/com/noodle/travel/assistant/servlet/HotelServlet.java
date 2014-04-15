dc153de388fc5b846b39c1cc5934cd8b441c8c6c
Tom <guan0078@gmail.com>
2014-04-15 22:43:27 +0800

    Implementing hotel servlet.

--- NoodleTravelAssistant/src/com/noodle/travel/assistant/servlet/HotelServlet.java
+++ NoodleTravelAssistant/src/com/noodle/travel/assistant/servlet/HotelServlet.java
@@ -1,6 +1,7 @@
 package com.noodle.travel.assistant.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
-import com.noodle.travel.assistant.util.HTMLUtils;
-import com.noodle.travel.assistant.util.PinyinUtils;
+import com.noodle.travel.assistant.baidu.pojo.PlaceSearchResults;
+import com.noodle.travel.assistant.baidu.pojo.Placesearchresponse;
+import com.noodle.travel.assistant.util.AllConstants;
+import com.noodle.travel.assistant.util.JaxbUtil;
+import com.noodle.travel.assistant.util.StreamUtil;
 
 public class HotelServlet extends HttpServlet {
 
@@ -25,68 +26,42 @@ public class HotelServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String keyword = req.getParameter("query");
-		System.out.println("input keyword = " + keyword);
+		String city = req.getParameter("query");
 		resp.setContentType("text/plain");
 		resp.setCharacterEncoding("UTF-8");
-		String result = process(keyword);
+		String result = process(city);
 		System.out.println(result);
 		resp.getWriter().println(result);
 	}
 
-	private static String process(String keyword) {
+	private static String process(String city) {
 		try {
-			http://api.map.baidu.com/place/v2/search?&q=%E9%A5%AD%E5%BA%97&region=%E5%8C%97%E4%BA%AC&output=json&ak=E4805d16520de693a3fe707cdc962045
-			
-			
-			if(StringUtils.isNotEmpty(keyword)){
-				String html = HTMLUtils
-						.getHtml("http://api.map.baidu.com/place/v2/search?&output=xml&ak=l0wKGnh4oTlA9DyRstrMy3QG&q="
-								+ java.net.URLEncoder.encode("酒店", "utf-8")
-								+ "&region=")
-								+ java.net.URLEncoder.encode("上海", "utf-8");
-				if (StringUtils.isNotEmpty(html)) {
-					String result = doParse(html);
-
-//					if (StringUtils.isEmpty(result)) {
-//						PinyinUtils pu = new PinyinUtils();
-//						html = HTMLUtils.getHtml("http://lvyou.baidu.com/"
-//								+ pu.getStringPinYin(keyword));
-//						result = doParse(html);
-//					}
-					if (StringUtils.isNotEmpty(result)) {
-						return result;
+			if(StringUtils.isNotEmpty(city)){
+				String str = StreamUtil.url2String(AllConstants.BAIDU_PLACE_SEARCH.replace("query", "酒店").replace("city", city));
+				if (StringUtils.isNotEmpty(str)) {
+					Placesearchresponse response = JaxbUtil.converyToJavaBean(str.toLowerCase(), Placesearchresponse.class);
+					if(response != null && response.getResults() != null && response.getResults().size()>0) {
+						List<PlaceSearchResults> results = response.getResults();
+						if(results != null && results.size()>0) {
+							StringBuilder sb = new StringBuilder();
+							int i=1;
+							for (PlaceSearchResults result : results) {
+								sb.append(i).append(":").append(result.getAddress()).append("\r\n").append(result.getTelephone()).append("\r\n");
+								i++;
+							}
+							return sb.toString();
+						}
 					}
 				}
 			}
-			return "很遗憾，没能找到大家对该景点的印象";
+			return "很遗憾，没能找到酒店信息";
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return "很遗憾，没能找到大家对该景点的印象";
-	}
-
-	private static String doParse(String html) {
-		StringBuilder sb = new StringBuilder();
-		Document doc = Jsoup.parse(html);
-		Elements ps = doc.select("p");
-		for (int i = 0; i < ps.size(); i++) {
-			if (ps.get(i).hasClass("text-desc-p")) {
-				sb.append(ps.get(i).text() + "\r\n");
-			}
-		}
-		if (StringUtils.isEmpty(sb.toString())) {
-			ps = doc.select("span");
-			for (int i = 0; i < ps.size(); i++) {
-				if (ps.get(i).hasClass("view-head-scene-abstract")) {
-					sb.append(ps.get(i).text());
-				}
-			}
-		}
-		return sb.toString();
+		return "很遗憾，没能找到酒店信息";
 	}
 
 	public static void main(String[] args) {
-		System.out.println(process("东方明珠"));
+		System.out.println(process("上海"));
 	}
 }
--- NoodleTravelAssistant/src/com/noodle/travel/assistant/util/AllConstants.java
+++ NoodleTravelAssistant/src/com/noodle/travel/assistant/util/AllConstants.java
@@ -1,7 +1,10 @@
 package com.noodle.travel.assistant.util;
 
 public class AllConstants {
-
+	
+	public static final String BAIDU_PLACE_SEARCH="http://api.map.baidu.com/place/v2/search?page_size=5&output=xml&ak=l0wKGnh4oTlA9DyRstrMy3QG&q=query&region=city";
+	public static final String BAIDU_PLACE_DETAIL="http://api.map.baidu.com/place/v2/detail?output=xml&uid=myuid&ak=l0wKGnh4oTlA9DyRstrMy3QG";
+	
 	public static final String WEIXIN_IMAGE_MESSAGE = "<xml><MsgType>image</MsgType> <PicUrl>noodlePicUrl</PicUrl></xml>";
 	public static final String WEIXIN_TEXT_MESSAGE = "<xml><MsgType>text</MsgType><Content>noodleTextContext</Content></xml>";
 	public static final String WEI_XIN_IMAGE_ITEM = "<item><Title>noodelImageTitle</Title><Description></Description><PicUrl>noodlePicUrl</PicUrl><Url>noodleClickUrl</Url></item>";
@@ -14,4 +17,66 @@ public class AllConstants {
 	public static final String NOODLE_IMAGE_DESC = "noodelImageNumber";
 	public static final String NOODLE_IMAGE_CLICK_URL = "noodleClickUrl";
 	public static final String NOODLE_TEXT_CONTEXT = "noodleTextContext";
+	
+	//微信消息常量-------------start
+	/**
+	 * 返回消息类型：文本
+	 */
+	public static final String RESP_MESSAGE_TYPE_TEXT = "text";
+
+	/**
+	 * 返回消息类型：音乐
+	 */
+	public static final String RESP_MESSAGE_TYPE_MUSIC = "music";
+
+	/**
+	 * 返回消息类型：图文
+	 */
+	public static final String RESP_MESSAGE_TYPE_NEWS = "news";
+
+	/**
+	 * 请求消息类型：文本
+	 */
+	public static final String REQ_MESSAGE_TYPE_TEXT = "text";
+
+	/**
+	 * 请求消息类型：图片
+	 */
+	public static final String REQ_MESSAGE_TYPE_IMAGE = "image";
+
+	/**
+	 * 请求消息类型：链接
+	 */
+	public static final String REQ_MESSAGE_TYPE_LINK = "link";
+
+	/**
+	 * 请求消息类型：地理位置
+	 */
+	public static final String REQ_MESSAGE_TYPE_LOCATION = "location";
+
+	/**
+	 * 请求消息类型：音频
+	 */
+	public static final String REQ_MESSAGE_TYPE_VOICE = "voice";
+
+	/**
+	 * 请求消息类型：推送
+	 */
+	public static final String REQ_MESSAGE_TYPE_EVENT = "event";
+
+	/**
+	 * 事件类型：subscribe(订阅)
+	 */
+	public static final String EVENT_TYPE_SUBSCRIBE = "subscribe";
+
+	/**
+	 * 事件类型：unsubscribe(取消订阅)
+	 */
+	public static final String EVENT_TYPE_UNSUBSCRIBE = "unsubscribe";
+
+	/**
+	 * 事件类型：CLICK(自定义菜单点击事件)
+	 */
+	public static final String EVENT_TYPE_CLICK = "CLICK";
+	//微信消息常量-------------end
 }
